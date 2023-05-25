c32dca86f209932eb1594ec438c24705b415d701
hhs0001 <heitorstein@gmail.com>
2023-05-24 22:24:04 -0300

    teste

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,12 +3,14 @@ import { NavbarWrapper } from "../components/navbar/Navbar";
 import { Content } from "../components/home/content";
 
 export default function Home() {
-  return (
-    <>
-      <NavbarWrapper children={null} />
-      <Container>
-        <Content />
-      </Container>
-    </>
-  );
-}
+   return (
+     <>
+       <NavbarWrapper>
+         {/* Aqui são colocados os elementos filhos que serão renderizados pela NavbarWrapper */}
+       </NavbarWrapper>
+        <Container>
+            <Content/>
+        </Container>
+     </>
+   );
+ }

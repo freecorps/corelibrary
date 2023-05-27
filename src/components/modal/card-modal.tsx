b171437c9b36536483aa532e949fb0e7ae6e9d8e
wolf-stievano <wolf.sachso@gmail.com>
2023-05-26 21:54:17 -0300

    card modal 100% funfante

--- src/components/modal/card-modal.tsx
+++ src/components/modal/card-modal.tsx
@@ -18,62 +18,58 @@ export default function CardModal(props: BookCardProps) {
                 + Mais
             </Button>
             <Modal  
-                scroll
                 width="600px"
                 aria-labelledby="modal-title"
                 aria-describedby="modal-description"
                 {...bindings}
             >
-                <div style={{ maxHeight: '80vh', overflowY: 'auto' }}>
-                    <Grid.Container gap={2} justify="center">
-                        <Grid xs={12} sm={6}>
-                            <Card css={{
-                                borderRadius: '$xl',
-                                display: 'flex',
-                                flexDirection: 'column',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                overflow: 'hidden'
-                            }}>
-                                <Image
-                                    src={props.imageUrl}
-                                    width="100%"
-                                    height="100%"
-                                    alt="Book Image"
-                                    style={{objectFit: 'cover', maxWidth: '100%', maxHeight: '100%'}}
-                                />
-                            </Card>
-                        </Grid>
-                        <Grid xs={12} sm={6}>
-                            <Card>
-                                <Modal.Header>
-                                    <Text id="modal-title" size={18}>
-                                        Titulo: {props.title}
-                                    </Text>
-                                </Modal.Header>
-                                <Card.Divider />
-                                <Modal.Body>
-                                    <Text id="modal-description" size={14}>
-                                        Autor: {props.author}
-                                    </Text> 
-                                    <Text id="modal-description" size={14}>
-                                        Descricao: {props.resume}
-                                    </Text>
-                                    <Text id="modal-description" size={14}>
-                                        Quantidade: {props.quantity}
-                                    </Text>
-                                </Modal.Body>
-                                <Card.Divider />
-                                <Modal.Footer justify="center">
-                                    <Button auto flat color="error" onPress={() => setVisible(false)}>
-                                        Close
-                                    </Button>
-                                    <EditModal />
-                                </Modal.Footer>
-                            </Card>
-                        </Grid>
-                    </Grid.Container>
-                </div>
+                <Grid.Container gap={2} justify="center">
+                    <Grid xs={12} sm={6}>
+                        <Card css={{
+                            borderRadius: '$xl',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            overflow: 'hidden'
+                        }}>
+                            <Image
+                                src={props.imageUrl}
+                                width="100%"
+                                height="100%"
+                                alt="Book Image"
+                                style={{objectFit: 'cover', maxWidth: '100%', maxHeight: '100%'}}
+                            />
+                        </Card>
+                    </Grid>
+                    <Grid xs={12} sm={6}>
+                        <Card>
+                            <Modal.Header>
+                                <Text id="modal-title" size={18}>
+                                    Titulo: {props.title}
+                                </Text>
+                            </Modal.Header>
+                            <Modal.Body style={{ overflowY: 'auto', maxHeight: 'calc(60vh - 50px)' }}>
+                                <Text id="modal-description" size={14}>
+                                    Autor: {props.author}
+                                </Text> 
+                                <Text id="modal-description" size={14}>
+                                    Descricao: {props.resume}
+                                </Text>
+                                <Text id="modal-description" size={14}>
+                                    Quantidade: {props.quantity}
+                                </Text>
+                            </Modal.Body>
+                        </Card>
+                    </Grid>
+                </Grid.Container>
+
+                <Modal.Footer>
+                    <Button auto flat color="error" onPress={() => setVisible(false)}>
+                        Close
+                    </Button>
+                    <EditModal />
+                </Modal.Footer>
             </Modal>
         </div>
     );

bc1a1232551227abe158943967a7244b7ff58205
wolf-stievano <wolf.sachso@gmail.com>
2023-05-28 17:17:13 -0300

    user edit modal

---
+++ src/components/modal/user-edit-modal.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { Modal, Button, Text, Input, Row, Checkbox, Tooltip} from "@nextui-org/react";
+import { IconButton } from "../icons/table/icon-button";
+import { EditIcon } from "../icons/table/edit-icon";
+
+export default function UserEditModal() {
+  const [visible, setVisible] = React.useState(false);
+  const handler = () => setVisible(true);
+
+  const closeHandler = () => {
+    setVisible(false);
+    console.log("closed");
+  };
+
+  return (
+    <div>
+      <Button bordered color="primary" auto onPress={handler}>
+      <Tooltip content="Edit user">
+          <IconButton onClick={() => console.log("Edit user")}>
+            <EditIcon size={20} fill="#979797" />
+          </IconButton>
+        </Tooltip>
+      </Button>
+      <Modal
+        closeButton
+        aria-labelledby="modal-title"
+        open={visible}
+        onClose={closeHandler}
+      >
+        <Modal.Header>
+          <Text id="modal-title" size={18}>
+            Welcome to
+            <Text b size={18}>
+              NextUI
+            </Text>
+          </Text>
+        </Modal.Header>
+        <Modal.Body>
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Email"
+          />
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Password"
+          />
+          <Row justify="space-between">
+            <Checkbox>
+              <Text size={14}>Remember me</Text>
+            </Checkbox>
+            <Text size={14}>Forgot password?</Text>
+          </Row>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button auto flat color="error" onPress={closeHandler}>
+            Close
+          </Button>
+          <Button auto onPress={closeHandler}>
+            Sign in
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
